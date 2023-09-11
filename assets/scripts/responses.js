c0d48b1827d7cee519b1624464c1fc4b021ee2a1
am224ut <94832146+Albion007@users.noreply.github.com>
2023-09-11 13:11:01 +0200

    chat bottttt

--- assets/scripts/responses.js
+++ assets/scripts/responses.js
@@ -33,17 +33,38 @@ function getBotResponse(input) {
     } else if (input == "Mire") {
         return "Gëzohem që jeni mirë!";
 
-        } else if (input == "mire") {
+    } else if (input == "mire") {
         return "Gëzohem që jeni mirë!";
+
+    } else if (input == "Si po kaloni?") {
+        return "Mirë, po ju?";
+
+    } else if (input == "si po kaloni") {
+        return "Mirë, po ju?";
     
     } else if (input == "Pune te mbare") {
         return "SHIHEMI! Faleminderit që na kontaktuat. Ju mirëpresim!";
+
+    } else if (input == "Sa kushton nje qethje?") {
+        return "Faleminderit që na kontaktuat. Prerje dhe formim floku 2 euro, Stylist Shaving vetëm 1 euro, Ngjyrë flokëve & Larje 3 euro dhe të gjitha çmimet tjera mund t'i gjeni tek menyja <b>çmimet</b> Ju mirëpresim!";
     
-    } else if (input == "Pune te mbare") {
-        return "SHIHEMI! Faleminderit që na kontaktuat";
+    } else if (input == "sa kushton nje qethje?") {
+        return "Faleminderit që na kontaktuat. Prerje dhe formim floku 2 euro, Stylist Shaving vetëm 1 euro, Ngjyrë flokëve & Larje 3 euro dhe të gjitha çmimet tjera mund t'i gjeni tek menyja <b>çmimet</b> Ju mirëpresim!";
+    
+    } else if (input == "A mundesh me mi tregu cmimet?") {
+        return "Faleminderit që na kontaktuat. Prerje dhe formim floku 2 euro, Stylist Shaving vetëm 1 euro, Ngjyrë flokëve & Larje 3 euro dhe të gjitha çmimet tjera mund t'i gjeni tek menyja <b>çmimet</b> Ju mirëpresim!";
 
     } else if (input == "pune te mbare") {
         return "SHIHEMI! Faleminderit që na kontaktuat";
+
+    } else if (input == "Kaloni mire") {
+        return "Gjithashtu! SHIHEMI! Faleminderit që na kontaktuat";
+
+    } else if (input == "A ka zbritje") {
+        return "Jo, zbritje nuk ka, por shërbim cilësor kemi! Faleminderit!";
+
+    } else if (input == "a ka zbritje") {
+        return "Jo, zbritje nuk ka, por shërbim cilësor kemi! Faleminderit!";
     
     } else {
         return "Ju mund të më pyesni gjithçka rreth shërbimeve tona!";
