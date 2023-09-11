6f10bb0f1bef1182ae5a71df8d16138813adfa83
am224ut <94832146+Albion007@users.noreply.github.com>
2023-09-11 12:08:23 +0200

    chat bottttt

--- assets/scripts/responses.js
+++ assets/scripts/responses.js
@@ -11,11 +11,38 @@ function getBotResponse(input) {
     // Simple responses
     if (input == "Pershendetje") {
         return "Përshëndetje! Si mund të ju ndihmojë?";
+
+    } else if (input == "Hi") {
+        return "Përshëndetje! Si mund të ju ndihmojë?!";
+
+    } else if (input == "Hello") {
+        return "Përshëndetje! Si mund të ju ndihmojë?!";
+
+    } else if (input == "pershendetje") {
+        return "Përshëndetje! Si mund të ju ndihmojë?!";
+    
+    } else if (input == "Qysh je?") {
+        return "Mirë faleminderit, ju si jeni?";
+
+    } else if (input == "qysh je") {
+        return "Mirë faleminderit! Ju si jeni?";
+
+    } else if (input == "Si je?") {
+        return "Mirë faleminderit, ju si jeni?";
+
+    } else if (input == "Mire") {
+        return "Gëzohem që jeni mirë!";
+
+        } else if (input == "mire") {
+        return "Gëzohem që jeni mirë!";
     
     } else if (input == "Pune te mbare") {
         return "SHIHEMI! Faleminderit që na kontaktuat. Ju mirëpresim!";
     
     } else if (input == "Pune te mbare") {
+        return "SHIHEMI! Faleminderit që na kontaktuat";
+
+    } else if (input == "pune te mbare") {
         return "SHIHEMI! Faleminderit që na kontaktuat";
     
     } else {
